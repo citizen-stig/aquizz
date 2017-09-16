15ccac6b026bec399d95e21b17c7621fc56a933f
Nikolai Golub <nikolai.golub@booking.com>
2017-09-16 18:39:14 +0200

    Update gitignore

--- aquizz/settings/develop.py
+++ aquizz/settings/develop.py
@@ -4,4 +4,4 @@ from .base import Config as BaseConfig
 class Config(BaseConfig):
     DEBUG = True
     SECRET_KEY = 'super-secret'
-    SECURITY_PASSWORD_SALT = 'some_salte'
+    SECURITY_PASSWORD_SALT = 'some_salt'
