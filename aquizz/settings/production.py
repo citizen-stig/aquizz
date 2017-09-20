568eb576da123386c6c112408803d57b6f82119b
Nikolay Golub <nikolay.v.golub@gmail.com>
2017-09-20 22:24:13 +0200

    Update production settings again

--- aquizz/settings/production.py
+++ aquizz/settings/production.py
@@ -1,5 +1,19 @@
 import os
 from .base import Config as BaseConfig
+from pymongo.uri_parser import parse_uri
+
+
+def get_mongodb_settings():
+    connection_string = os.environ['MONGODB_URI']
+    parsed = parse_uri(connection_string)
+    first_node = parsed['nodelist'][0]
+    host, port = first_node
+    return {
+        'host': host,
+        'port': port,
+        'db': parsed['database'],
+        'username': parsed['username'],
+        'password': parsed['password']}
 
 
 class Config(BaseConfig):
@@ -7,5 +21,5 @@ class Config(BaseConfig):
     Testing = False
     SECRET_KEY = os.environ['FLASK_SECRET_KEY']
     SECURITY_PASSWORD_SALT = os.environ['FLASK_PASSWORD_SALT']
-    MONGODB_SETTINGS = os.environ['MONGODB_URI']
+    MONGODB_SETTINGS = get_mongodb_settings()
 
